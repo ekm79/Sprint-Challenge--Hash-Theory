c63501e3b569ba606da37eea7f022b61501dfa78
Erin Meredith <ekmeredith@att.net>
2018-10-12 13:37:18 -0400

    ex1 passing

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -1,17 +1,18 @@
 def get_indices_of_item_weights(weights, limit):
   weight_list = {}
   for i, weight in enumerate(weights):
-    weight_list[weight] = i
-  for i, weight in enumerate(weights):
-
-    if limit - weight in weight_list:
-      diff = weight_list[limit - weight]
+    weight_list.update({weight: i})
+  print(weight_list)
+  for i, weight in enumerate(weight_list):
+    diff = limit - weight
+    print (diff)
+    if diff in weight_list:
+      diff = weight_list[diff]
       if i > diff:
         return (i, diff)
       else:
         return (diff, i)
-    else:
-      return ()
+  return ()
 
 
 if __name__ == '__main__':
