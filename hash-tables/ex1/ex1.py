cc7140ec49b2d51fcc2e6f7c1c78cf657f8d7c78
Erin Meredith <ekmeredith@att.net>
2018-10-12 12:54:16 -0400

    hash tables completed, not passing yet

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -1,6 +1,21 @@
 def get_indices_of_item_weights(weights, limit):
-  pass 
+  weight_list = {}
+  for i, weight in enumerate(weights):
+    weight_list[weight] = i
+  for i, weight in enumerate(weights):
+
+    if limit - weight in weight_list:
+      diff = weight_list[limit - weight]
+      if i > diff:
+        return (i, diff)
+      else:
+        return (diff, i)
+    else:
+      return ()
+
 
 if __name__ == '__main__':
   # You can write code here to test your implementation using the Python repl
+  
+
   pass 
