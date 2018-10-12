94014afccdfa6ab96babcf60fafb429eafcdd4b7
Erin Meredith <ekmeredith@att.net>
2018-10-12 13:59:11 -0400

    ex2 not passing yet

--- hash-tables/ex2/ex2.py
+++ hash-tables/ex2/ex2.py
@@ -5,18 +5,30 @@ def reconstruct_trip(tickets):
   for i in tickets:
     src = i[0]
     dest = i[1]
-    #h = hash(i[0])
-    tix[src]: dest
+    tix.update({src: dest})
   current_ticket = tix[None]
   for j in tix:
-    if j[0] == current_ticket[1]:
-      next_ticket = j[0]
+    if tix[j] == current_ticket:
+      next_ticket = j
   while current_ticket != None:
     route.append(current_ticket)
     current_ticket = next_ticket
- 
+  print(route)
   return route
 
+tickets = [
+  ('PIT', 'ORD'),
+  ('XNA', 'CID'),
+  ('SFO', 'BHM'),
+  ('FLG', 'XNA'),
+  (None, 'LAX'), 
+  ('LAX', 'SFO'),
+  ('CID', 'SLC'),
+  ('ORD', None),
+  ('SLC', 'PIT'),
+  ('BHM', 'FLG'),
+]
+reconstruct_trip(tickets)
 
 if __name__ == '__main__':
   # You can write code here to test your implementation using the Python repl
